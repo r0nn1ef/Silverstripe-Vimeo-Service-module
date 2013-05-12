ccc580c99588d91a439fbee982bc93ec171d14f4
Ron Ferguson <r0nn1ef8580@gmail.com>
2013-05-11 23:30:56 -0500

    added SiteConfig decorator for VimeoSiteConfig DataExtension

--- _config.php
+++ _config.php
@@ -1,8 +1,10 @@
 <?php
+	// Decorate SiteConfig to hold Vimeo API keys.
+	Object::add_extension('SiteConfig', 'VimeoSiteConfig');
 	// Decorate StringField so text-based data types have the method URLEncode
 	// for use in templates.
 	Object::add_extension('StringField', 'StringFieldDOD');
-	
+
 	// adds a simple shortcode parser to include a specific vimeo video in text.
 	ShortcodeParser::get('default')->register('vimeo', array('VimeoGalleryPage', 'VimeoShortcodeHandler'));
 ?>
