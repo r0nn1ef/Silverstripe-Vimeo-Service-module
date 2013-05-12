112f49cd8a7d71b274e4f924edf280304c7ec615
Ron Ferguson <r0nn1ef8580@gmail.com>
2013-05-11 23:32:06 -0500

    misc. updates for new SS 3 framework

--- code/VimeoGalleryPage.php
+++ code/VimeoGalleryPage.php
@@ -2,11 +2,11 @@
 
 
 class VimeoGalleryPage extends Page {
-	
+
 	public static $singular_name = 'Vimeo Gallery Page';
-	
+
 	public static $plural_name = 'Vimeo Gallery Pages';
-	
+
 	public static $db = array(
 		'Method' => 'Int',
 		'User' => 'Varchar(100)',
@@ -17,7 +17,7 @@ class VimeoGalleryPage extends Page {
 		'PopupHeight' => 'Int',
 		'SortField' => 'Varchar(100)'
 	);
-	
+
 	public static $defaults = array(
 		'Method' => 1,
 		'ShowVideoInPopup' => true,
@@ -27,43 +27,45 @@ class VimeoGalleryPage extends Page {
 		'PopupHeight' => 225,
 		'SortField' => 'UploadDate'
 	);
-	
+
 	public static $allowed_children = array();
-	
+
 	public static $icon = 'vimeoservice/images/vimeo';
-	
+
 	protected $_cachedVideos = null;
-	
+
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-		
-		$fields->addFieldToTab('Root.Content.Videos', new DropdownField("Method", _t('VimeoGalleryPage.SELECT', "Select"), array(
+
+		// $fields->findOrMakeTab("Root.Vimeo", _t('VimeoGalleryPage.SETTINGS', "Vimeo"));
+		// var_dump($tab);exit;
+		$fields->addFieldToTab('Root.Vimeo', new DropdownField("Method", _t('VimeoGalleryPage.SELECT', "Select"), array(
 			'1' => _t('VimeoGalleryPage.USER', 'User'),
 			'2' => _t('VimeoGalleryPage.GROUP', 'Group'),
 			'3' => _t('VimeoGalleryPage.ALBUM', 'Album')
 		)));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new TextField("User", _t('VimeoGalleryPage.USER_ID_LABEL', "Vimeo Username/Vimeo Group Name/Vimeo Album ID")));
-		
-		$fields->addFieldsToTab("Root.Content.Videos", new DropdownField("VideosPerPage", _t('VimeoGalleryPage.VIDEOS_PER_PAGE', "Number of videos per page"), array(
+
+		$fields->addFieldToTab("Root.Vimeo", new TextField("User", _t('VimeoGalleryPage.USER_ID_LABEL', "Vimeo Username/Vimeo Group Name/Vimeo Album ID")));
+
+		$fields->addFieldsToTab("Root.Vimeo", new DropdownField("VideosPerPage", _t('VimeoGalleryPage.VIDEOS_PER_PAGE', "Number of videos per page"), array(
 			'10' => '10',
 			'20' => '20',
 			'30' => '30',
 			'40' => '40',
-			'50' => '50'	
+			'50' => '50'
 		)));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new DropdownField("SortField", _t('VimeoGalleryPage.SORT_BY', "Sort by"), array(
+
+		$fields->addFieldToTab("Root.Vimeo", new DropdownField("SortField", _t('VimeoGalleryPage.SORT_BY', "Sort by"), array(
 			'newest' => _t('VimeoGalleryPage.NEWEST', 'Newest'),
 			'oldest' => _t('VimeoGalleryPage.OLDEST', 'Oldest'),
 			'most_played' => _t('VimeoGalleryPage.MOST_PLAYED', 'Most played'),
 			'most_commented' => _t('VimeoGalleryPage.MOST_COMMENTED', 'Most commented'),
 			'most_liked' => _t('VimeoGalleryPage.MOST_LIKED', 'Most liked')
 		)));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new CheckboxField("ShowVideoInPopup", _t('VimeoGalleryPage.VIDEO_IN_POPUP', "Show video in popup?")));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new DropdownField("PopupTheme", _t('VimeoGalleryPage.POPUP_THEME', "Popup theme"), array(
+
+		$fields->addFieldToTab("Root.Vimeo", new CheckboxField("ShowVideoInPopup", _t('VimeoGalleryPage.VIDEO_IN_POPUP', "Show video in popup?")));
+
+		$fields->addFieldToTab("Root.Vimeo", new DropdownField("PopupTheme", _t('VimeoGalleryPage.POPUP_THEME', "Popup theme"), array(
 			'default' => _t('VimeoGalleryPage.DEFAULT', 'Default'),
 			'dark_square' => _t('VimeoGalleryPage.DARK_SQUARE', 'Dark Square'),
 			'dark_rounded' => _t('VimeoGalleryPage.DARK_ROUNDED', 'Dark Rounded'),
@@ -71,19 +73,21 @@ class VimeoGalleryPage extends Page {
 			'light_rounded' => _t('VimeoGalleryPage.LIGHT_ROUNDED', 'Light Rounded'),
 			'facebook' => _t('VimeoGalleryPage.FACEBOOK', 'Facebook')
 		)));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new Textfield("PopupWidth", _t('VimeoGalleryPage.POPUP_WIDTH', "Popup width")));
-		
-		$fields->addFieldToTab("Root.Content.Videos", new Textfield("PopupHeight", _t('VimeoGalleryPage.POPUP_HEIGHT', "Popup height")));
-		
+
+		$fields->addFieldToTab("Root.Vimeo", new Textfield("PopupWidth", _t('VimeoGalleryPage.POPUP_WIDTH', "Popup width")));
+
+		$fields->addFieldToTab("Root.Vimeo", new Textfield("PopupHeight", _t('VimeoGalleryPage.POPUP_HEIGHT', "Popup height")));
+
 		return $fields;
 	}
-	
+
 	function VimeoVideos() {
-		
+
 		if($this->_cachedVideos) return $this->_cachedVideos;
-		
-		$vimeo = new VimeoService();
+
+		$config = SiteConfig::current_site_config();
+
+		$vimeo = new VimeoService($config->VimeoAPIKey, $config->VimeoSecretKey);
 		$start = isset($_GET['start']) ? (int)$_GET['start'] : 0;
 		$per_page = intval($this->VideosPerPage) < 10 ? 10 : intval($this->VideosPerPage);
 		switch($this->Method) {
@@ -99,43 +103,43 @@ class VimeoGalleryPage extends Page {
 			default:
 				return false;
 		}
-		
+
 		//$sort_direction = ($this->SortField == 'Title' ? 'ASC' : 'DESC');
 		//$videos->sort($this->SortField, $sort_direction);
-		
+
 		$this->_cachedVideos = $videos;
-		
+
 		return $videos;
 	}
-	
+
 	function flushCache() {
 		parent::flushCache();
 		unset($this->_cachedVideos);
 	}
-	
+
 	public static function VimeoShortcodeHandler($attributes, $content=null, $parser=null) {
 		if(!isset($attributes['id'])) return '';
 		$width = isset($attributes['width']) ? intval($attributes['width']) : VimeoService::getDefaultWidth();
 		$height = isset($attributes['height']) ? intval($attributes['height']) : VimeoService::getDefaultHeight();
 		if($width == 0) $width = VimeoService::getDefaultWidth();
 		if($height == 0) $width = VimeoService::getDefaultHeight();
-		
+
 		return "<iframe src='http://player.vimeo.com/video/{$attributes['id']}' width='{$width}' height='{$height}' frameborder='0'></iframe>";
 	}
-	
+
 }
 
 class VimeoGalleryPage_Controller extends Page_Controller {
-	
+
 	static $allowed_actions = array(
 				'index',
 				'view'
 			);
-	
+
 	// Since we are pulling the video data from a remote service, we'll want to store the video title
-	// for use in the breadcrumbs later.		
+	// for use in the breadcrumbs later.
 	protected $_videoTitle;
-	
+
 	function init() {
 		if(Director::fileExists(project() . "/css/VimeoGallery.css")) {
 			Requirements::css(project() . "/css/VimeoGallery.css");
@@ -146,17 +150,17 @@ class VimeoGalleryPage_Controller extends Page_Controller {
 		}
 
 		// only include if necessary
-		
+
 		if($this->ShowVideoInPopup) {
-			
+
 			Requirements::javascript( "vimeoservice/javascript/jquery-1.4.4.min.js" );
 			Requirements::javascript( "vimeoservice/javascript/jquery.prettyPhoto.js" );
 			Requirements::css('vimeoservice/css/prettyPhoto.css');
-			
+
 			$theme = $this->PopupTheme ? $this->PopupTheme : 'default';
 			$width = $this->PopupWidth < 1 ? 400 : $this->PopupWidth;
 			$height = $this->PopupHeight < 1 ? 225 : $this->PopupHeight;
-			
+
 			Requirements::customScript(<<<JS
 $(document).ready(function(){
 $("a[rel^='prettyPhoto']").prettyPhoto({
@@ -167,33 +171,34 @@ default_height: $height,
 });
 JS
 			);
-			
+
 		}
-		
+
 		parent::init();
 	}
-	
+
 	function isUserRequest() {
 		return $this->Method == 1 ? true : false;
 	}
-	
+
 	function isGroupRequest() {
 		return $this->Method == 2 ? true : false;
 	}
-	
+
 	function getVideo() {
 		$params = $this->getURLParams();
 		if(is_numeric($params['ID'])) {
-			$vimeo = new VimeoService();
+			$config = SiteConfig::current_site_config();
+			$vimeo = new VimeoService($config->VimeoAPIKey, $config->VimeoSecretKey);
 			$video = $vimeo->getVideoById($params['ID']);
 			return $video;
 		} else {
 			return false;
 		}
 	}
-	
+
 	function view() {
-		
+
 		if($video = $this->getVideo()) {
 			$this->_videoTitle = $video->Title->getValue();
 			$data = array('Video' => $video);
@@ -202,17 +207,19 @@ JS
 			return $this->httpError(404, _t('VimeoGalleryPage.VIDEO_NOT_FOUND', 'Sorry that video could not be found.'));
 		}
 	}
-	
+
 	///////////////////////// Page control functions ////////////////////////////
-	
-	
+
+
 	function Breadcrumbs() {
+		return FALSE;
+		/*
 		//Get the default breadcrumbs
         $Breadcrumbs = parent::Breadcrumbs();
-		
+
 		//Explode them into their individual parts
         $Parts = explode(SiteTree::$breadcrumbs_delimiter, $Breadcrumbs);
-		
+
 		// If we are viewing a single video, add link back to the index action of this controller
 		// and add the video title to the breadcrumbs.
 		$params = $this->getURLParams();
@@ -221,10 +228,12 @@ JS
 			$Parts[$lastIdx] = '<a href="' . $this->Link() . '">' . $Parts[$lastIdx] . '</a>';
 			$Parts[] = !$this->_videoTitle ? _t('VimeoGalleryPage.UNTITLED_VIDEO', 'Untitled Video') : $this->_videoTitle;
 		}
-		
+
 		//Return the imploded array
         $Breadcrumbs = implode(SiteTree::$breadcrumbs_delimiter, $Parts);
-		
+
 		return $Breadcrumbs;
+		 *
+		 */
 	}
 }
